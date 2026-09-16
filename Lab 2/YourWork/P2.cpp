4b1ac5fadbd0ab5033b34a7ce6ac72be87fbde4a
mashuranadia29-design <mashuranadia29@gmail.com>
2026-09-16 11:00:12 +0600

    Add files via upload

---
+++ Lab 2/YourWork/P2.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Address{
+
+string city;
+int zipcode;
+};
+
+    struct Student{
+    int id;
+    string name;
+    Address studentAddress;
+};
+
+    int main()
+    {
+        Student s1;
+        s1.id=102;
+        s1.name="Rahim";
+        s1.studentAddress.city="Sylhet";
+         s1.studentAddress.zipcode=3100;
+
+         cout<<s1.name<<" lives in "<< s1.studentAddress.city<<endl;
+    }
